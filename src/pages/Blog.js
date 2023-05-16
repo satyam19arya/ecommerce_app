7c74c655675117eed0e98602ee536679c7a11aef
Satyam <satyam19arya@gmail.com>
2023-05-16 16:01:42 +0530

    initial changes

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Helmet} from "react-helmet";
+import BreadCrumb from '../components/BreadCrumb';
+import BlogCard from '../components/BlogCard';
 
 const Blog = () => {
   return (
@@ -8,6 +10,40 @@ const Blog = () => {
         <meta charSet="utf-8" />
         <title>Blogs</title>
       </Helmet>
+      <BreadCrumb title="Blogs" />
+
+      <div className="blog-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                  <h3 className="filter-title">Find by Categories</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li>Watch</li>
+                      <li>Tv</li>
+                      <li>Camera</li>
+                      <li>Laptop</li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+
+            <div className="col-9">
+              <div className="row gap-13">
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
+import BreadCrumb from '../components/BreadCrumb';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_2crac8w', 'template_3uopgza', form.current, 'ZQdDY_A9RQX5fz5RM')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset();
+    };
+
   return (
-    <div>Contact</div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Contact</title>
+      </Helmet>
+      <BreadCrumb title="Contact" />
+
+      <section className='contact'>
+        <h3 className='contact-us'>Contact us</h3>
+        <div className='container contact_container'>
+            <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name='name' placeholder='Enter your name' required />
+                <input type="email" name='email' placeholder='Enter your email' required />
+                <textarea name="message" rows="7" placeholder="Your message" required></textarea>
+                <button type='submit' className='button'>Send Message</button>
+            </form>
+        </div>
+      </section>
+    </>
   )
 }
 
